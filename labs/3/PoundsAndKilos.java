f5b35a4433b34316d25ff598a389172433b2aff8
Juan Arias <juan.arias@marist.edu>
2018-02-11 21:36:01 -0500

    Feedback

--- labs/3/PoundsAndKilos.java
+++ labs/3/PoundsAndKilos.java
@@ -5,7 +5,7 @@ public class PoundsAndKilos {
         System.out.printf("\t\t|\t\t");
         System.out.printf("%-12s%-8s\n", "Pounds", "Kilograms");
         
-        for (int i = 1, j = 20; i < 200; i++, j+=5) {
+        for (int i = 1, j = 20; i < 200; i+=2, j+=5) { // JA: The first one grows by 2
             
             System.out.printf("%-12d%8.1f", i, i * 2.2);
             System.out.printf("\t\t|\t\t");
